aea3e9864889c19df6374b2eeccb0c189e86332f
nguyen-thanh-tung <nguyenthanhtung8196@gmail.com>
2026-04-08 22:06:21 +0900

    update test again

--- VHDL/test.vhd
+++ VHDL/test.vhd
@@ -1,28 +1,26 @@
 -- This is a commnent
 -- This is another comment
 
-library IEEE
-use IEEE.STD_LOGIC
+library IEEE;
+use IEEE.std_logic_1164.all;
 
 entity example is
 generic(
+	N : integer := 4
 );
 port (
-	My_input : in std_logic;
-	My_out : out std_logic_vector (31 downto 0);
+	My_out : in std_logic;
+	My_input : out std_logic_vector (31 downto 0)
 );
+end example;
 
-end example
 
-
-architecture test of example
+architecture test of example is
 begin
 -- Put your architecture code /design here
-map -- this is used in component instantiations
+-- map -- this is used in component instantiations
 
 My_input <= My_out(1);
-
-A <= B;
 
 end test;
 
