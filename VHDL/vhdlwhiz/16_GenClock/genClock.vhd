9cfbda0817201436e6e56d38c1ef993371b4b55a
nguyen-thanh-tung <nguyenthanhtung8196@gmail.com>
2026-06-03 22:36:50 +0900

    update

--- VHDL/vhdlwhiz/16_GenClock/genClock.vhd
+++ VHDL/vhdlwhiz/16_GenClock/genClock.vhd
@@ -11,8 +11,54 @@ end entity;
 
 -- Architecture
 architecture sim of genClock is
+-- component
+component FlipFlop is
+    port(
+        Clk     : in std_logic;
+        nRst    : in std_logic;
+        Input   : in std_logic;
+        Output  : out std_logic
+    );
+end component;
+
+-- Constant
+constant ClockFreq : integer := 100e6;
+constant ClockPeriod : time := 1000 ms / ClockFreq;
+-- Signal 
+signal Clk : std_logic := '1';
+signal nRst : std_logic := '0';
+signal Input : std_logic := '0';
+signal Output : std_logic;
+
 begin
-    
+    -- The Device Under Test (DUT)
+    i_FlipFlop : FlipFlop
+        port map(
+            Clk => Clk,
+            nRst => nRst,
+            Input => Input,
+            Output => Output
+        );
+
+    Clk <= not Clk after ClockPeriod / 2;
+
+    process is
+    begin
+        nRst <= '1';
+
+        wait for 20 ns;
+        Input <= '1';
+        wait for 22 ns;
+        Input <= '0';
+        wait for 6 ns;
+        Input <= '1';
+        wait for 20 ns;
+
+        nRst <= '0';
+
+        wait;
+    end process;
+
 end architecture;
 
 
